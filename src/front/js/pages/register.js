41a908e6ddf71656a3f0fbf54daa80d27ed50959
MiguelSEO1 <113799625+MiguelSEO1@users.noreply.github.com>
2023-04-01 19:14:09 +0200

    Update register.js

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -34,7 +34,7 @@ export const Register = () => {
   return (
     <div className="  container justify-content-center align-items-center mb-5">
       <div className="container-fluid text-center mt-5 border bg-warning text-danger fs-1">
-        PAGINA DE REGISTER
+        PÁGINA DE REGISTER
       </div>
 
       <form className=" border m-5 p-5">
